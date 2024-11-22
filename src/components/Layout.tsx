563c6bfd48f80c95e32c8ef2fa536ef93b22419f
Ada Lundhe <sean.corbett@umontana.edu>
2024-11-22 07:20:10 -0600

    AL: remove bottom seperator

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -15,9 +15,6 @@ export const Layout = ({
       <main className="row-span-2 w-full my-4 px-3 flex justify-center items-center h-full">
         {children}
       </main>
-      <div className="w-full flex justify-center">   
-        <Separator className="w-3/4"/>
-      </div>
     </>
   );
 }
