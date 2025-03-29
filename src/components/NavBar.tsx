3c412690d4b569864b4cc9f99f8ee0a5caba8889
Ada Lundhe <sean.corbett@umontana.edu>
2025-03-29 11:38:10 -0500

    AL: navbar styling fixes

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -41,7 +41,7 @@ export const NavBar = () => {
             </Avatar>
             <NavigationMenuItem className="h-[100px]">
                 <NavigationMenuTrigger className="lg:mx-[16px] hover:underline text-xl lg:w-[120px] w-[60px]" asChild>
-                    <div className="cursor-default flex items-center justify-center space-x-2 lg:hover:bg-gray-50 lg:focus:bg-gray-50">
+                    <div className="cursor-default flex items-center justify-center space-x-2 lg:hover:bg-gray-50 lg:lg:focus:bg-gray-50">
                         <CiMenuBurger 
                             className="text-2xl top-[1px] transition duration-300 group-data-[state=open]:rotate-90"
                             aria-hidden="true"
@@ -53,15 +53,15 @@ export const NavBar = () => {
                     <div className="border-t h-[100vh] lg:mt-[34px] w-[100vw] bg-white flex flex-col w-full no-scroll text-lg font-light">
                         <ScrollArea className="h-[60vh]">
                             <Accordion defaultValue="home" type="single" collapsible className="flex flex-col w-full">
-                                <AccordionItem value="home" className=" hover:bg-gray-50 focus:bg-gray-50 py-2 px-2 h-[40px] w-full flex grow-1 items-center space-x-2 hover:bg-gray-50 focus:bg-gray-50">
+                                <AccordionItem value="home" className=" lg:hover:bg-gray-50 lg:focus:bg-gray-50 py-2 px-2 h-[40px] w-full flex grow-1 items-center space-x-2 lg:hover:bg-gray-50 lg:focus:bg-gray-50">
                                     {/* <MdHome/> */}
                                     <NavigationMenuLink href="/" className={navigationMenuLinkStyle()}>
                                         home
                                     </NavigationMenuLink>
                                 </AccordionItem>
-                                <AccordionItem value="studio" className="px-2 hover:bg-gray-50 focus:bg-gray-50">
+                                <AccordionItem value="studio" className="px-2 lg:hover:bg-gray-50 lg:focus:bg-gray-50">
                                     <AccordionTrigger 
-                                        className="hover:bg-gray-50 focus:bg-gray-50 p-0 cursor-default py-2 text-lg font-light"
+                                        className="lg:hover:bg-gray-50 lg:focus:bg-gray-50 p-0 cursor-default py-2 text-lg font-light"
                                         chevronSide="left"
                                     >   
                                         <div className="flex items-center space-x-2 cursor-default">
@@ -76,7 +76,7 @@ export const NavBar = () => {
                                     <AccordionContent className="p-0">
                                         <Separator/>     
                                         <ul className="list-none pl-4">
-                                            <li className="w-full py-2 flex items-center space-x-2 grow-1 hover:bg-gray-50 focus:bg-gray-50"> 
+                                            <li className="w-full py-2 flex items-center space-x-2 grow-1 lg:hover:bg-gray-50 lg:focus:bg-gray-50"> 
                                                 {/* <PiInfoFill /> */}
                                                 <Link href="/about" className="cursor-pointer hover:underline" passHref>                     
                                                     <NavigationMenuLink className={navigationMenuLinkStyle()} >
@@ -85,7 +85,7 @@ export const NavBar = () => {
                                                 </Link>
                                             </li>
                                             <Separator/>
-                                            <li className="w-full py-2 flex items-center space-x-2 grow-1 hover:bg-gray-50 focus:bg-gray-50">
+                                            <li className="w-full py-2 flex items-center space-x-2 grow-1 lg:hover:bg-gray-50 lg:focus:bg-gray-50">
                                                 {/* <GiGearHammer /> */}
                                                 <NavigationMenuLink href="/gear" className={navigationMenuLinkStyle()}>
                                                     gear
@@ -94,8 +94,8 @@ export const NavBar = () => {
                                         </ul>
                                     </AccordionContent>
                                 </AccordionItem>
-                                {/* <AccordionItem value="services" className="px-2 hover:bg-gray-50 focus:bg-gray-50">
-                                    <AccordionTrigger className="hover:bg-gray-50 focus:bg-gray-50 p-0 cursor-default py-2">   
+                                {/* <AccordionItem value="services" className="px-2 lg:hover:bg-gray-50 lg:focus:bg-gray-50">
+                                    <AccordionTrigger className="lg:hover:bg-gray-50 lg:focus:bg-gray-50 p-0 cursor-default py-2">   
                                         <div className="flex items-center space-x-2 cursor-default">
                                             <PiFadersFill 
                                                 aria-hidden="true"
@@ -108,7 +108,7 @@ export const NavBar = () => {
                                     <AccordionContent className="p-0">
                                         <Separator/>     
                                         <ul className="list-none pl-4">
-                                            <li className="w-full py-2 flex items-center space-x-2 grow-1 hover:bg-gray-50 focus:bg-gray-50">
+                                            <li className="w-full py-2 flex items-center space-x-2 grow-1 lg:hover:bg-gray-50 lg:focus:bg-gray-50">
                                                 <TbDeviceTvOld/>
                                                 <Link href="/services/commercial_audio" className="cursor-pointer hover:underline" passHref>                                                
                                                     <NavigationMenuLink className={navigationMenuLinkStyle()}>
@@ -117,7 +117,7 @@ export const NavBar = () => {
                                                 </Link>
                                             </li>
                                             <Separator/>
-                                            <li className="w-full py-2 flex items-center space-x-2 grow-1 hover:bg-gray-50 focus:bg-gray-50">
+                                            <li className="w-full py-2 flex items-center space-x-2 grow-1 lg:hover:bg-gray-50 lg:focus:bg-gray-50">
                                                 <GiGrandPiano/>
                                                 <Link href="/services/composition" className="cursor-pointer hover:underline" passHref>                                                
                                                     <NavigationMenuLink className={navigationMenuLinkStyle()}>
@@ -126,7 +126,7 @@ export const NavBar = () => {
                                                 </Link>
                                             </li>
                                             <Separator/>
-                                            <li className="w-full py-2 flex items-center space-x-2 grow-1 hover:bg-gray-50 focus:bg-gray-50">
+                                            <li className="w-full py-2 flex items-center space-x-2 grow-1 lg:hover:bg-gray-50 lg:focus:bg-gray-50">
                                                 <BiSolidSpeaker/>
                                                 <Link href="/services/mastering" className="cursor-pointer hover:underline" passHref>                                                
                                                     <NavigationMenuLink className={navigationMenuLinkStyle()}>
@@ -135,7 +135,7 @@ export const NavBar = () => {
                                                 </Link>
                                             </li>
                                             <Separator/>
-                                            <li className="w-full py-2 flex items-center space-x-2 grow-1 hover:bg-gray-50 focus:bg-gray-50"> 
+                                            <li className="w-full py-2 flex items-center space-x-2 grow-1 lg:hover:bg-gray-50 lg:focus:bg-gray-50"> 
                                                 <GiDrumKit/>
                                                 <Link href="/services/mixing" className="cursor-pointer hover:underline" passHref>                                                
                                                     <NavigationMenuLink className={navigationMenuLinkStyle()}>
@@ -144,7 +144,7 @@ export const NavBar = () => {
                                                 </Link>
                                             </li>
                                             <Separator/>
-                                            <li className="w-full py-2 flex items-center space-x-2 grow-1 hover:bg-gray-50 focus:bg-gray-50">
+                                            <li className="w-full py-2 flex items-center space-x-2 grow-1 lg:hover:bg-gray-50 lg:focus:bg-gray-50">
                                                 <GiMusicalKeyboard/>
                                                 <Link href="/services/sound_design" className="cursor-pointer hover:underline" passHref>                                                
                                                     <NavigationMenuLink className={navigationMenuLinkStyle()}>
@@ -155,8 +155,8 @@ export const NavBar = () => {
                                         </ul>
                                     </AccordionContent>
                                 </AccordionItem> */}
-                                <AccordionItem value="legal" className="px-2 hover:bg-gray-50 focus:bg-gray-50">
-                                    <AccordionTrigger className="hover:bg-gray-50 focus:bg-gray-50 p-0 cursor-default py-2 text-lg font-light" chevronSide="left">   
+                                <AccordionItem value="legal" className="px-2 lg:hover:bg-gray-50 lg:focus:bg-gray-50">
+                                    <AccordionTrigger className="lg:hover:bg-gray-50 lg:focus:bg-gray-50 p-0 cursor-default py-2 text-lg font-light" chevronSide="left">   
                                         <div className="flex items-center space-x-2 cursor-default">
                                             {/* <LiaFileContractSolid 
                                                 aria-hidden="true"
@@ -169,14 +169,14 @@ export const NavBar = () => {
                                     <AccordionContent className="p-0">  
                                         <Separator/>        
                                         <ul className="list-none pl-4">
-                                            <li className="w-full py-2 flex items-center space-x-2 grow-1 hover:bg-gray-50 focus:bg-gray-50">
+                                            <li className="w-full py-2 flex items-center space-x-2 grow-1 lg:hover:bg-gray-50 lg:focus:bg-gray-50">
                                                 {/* <MdPolicy/> */}
                                                 <NavigationMenuLink href="/privacy" className={navigationMenuLinkStyle()}>
                                                     privacy
                                                 </NavigationMenuLink>
                                             </li>
                                             <Separator/>
-                                            <li className="w-full py-2 flex items-center space-x-2 grow-1 hover:bg-gray-50 focus:bg-gray-50">
+                                            <li className="w-full py-2 flex items-center space-x-2 grow-1 lg:hover:bg-gray-50 lg:focus:bg-gray-50">
                                                 {/* <LiaFileContractSolid/> */}
                                                 <NavigationMenuLink href="/terms_of_service" className={navigationMenuLinkStyle()}>
                                                     terms of service
@@ -185,7 +185,7 @@ export const NavBar = () => {
                                         </ul>
                                     </AccordionContent>
                                 </AccordionItem>
-                                <AccordionItem value="contact" className="hover:bg-gray-50 focus:bg-gray-50 py-2 px-2 h-[40px] w-full flex grow-1 items-center space-x-2 hover:bg-gray-50 focus:bg-gray-50">
+                                <AccordionItem value="contact" className="lg:hover:bg-gray-50 lg:focus:bg-gray-50 py-2 px-2 h-[40px] w-full flex grow-1 items-center space-x-2 lg:hover:bg-gray-50 lg:focus:bg-gray-50">
                                     {/* <IoMdContact/> */}
                                     <NavigationMenuLink href="/contact" className="cursor-pointer hover:underline">
                                         contact
