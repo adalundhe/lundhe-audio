2c7c93f415588cb3566e9d3004efcaec529b4334
Ada Lundhe <sean.corbett@umontana.edu>
2025-12-14 12:13:01 -0600

    AL: add signup options

--- src/app/api/users/[userId]/delete/route.ts
+++ src/app/api/users/[userId]/delete/route.ts
@@ -11,34 +11,26 @@ function userIdIsString(userId: string | string[] | undefined): userId is string
     return userId?.toString !== undefined
 }
  
-export default async function handler(
+export default async function DELETE(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
+    const { userId } = req.query
 
-    if (req.method === "DELETE") {
-
-        const { userId } = req.query
-
-        if (!userIdIsString(userId)){
-            return res.status(400).json({message:"Invalid userId"})
-        }
-
-        const headers = new Headers()
-        headers.append("Authorization", `Bearer ${env.CLERK_SECRET_KEY}`)
-        headers.append('Content-Type', 'application/json')
-        
-
-        return await fetch(`https://api.clerk.com/v1/users/${userId}`, {
-            method: "DELETE",
-            headers: headers,
-        })
-            .then(_ => res.status(200).json({ message: 'OK' }))
-            .catch(_ => res.status(500))
-        
-
+    if (!userIdIsString(userId)){
+        return res.status(400).json({message:"Invalid userId"})
     }
 
-    return res.status(405)
-  
+    const headers = new Headers()
+    headers.append("Authorization", `Bearer ${env.CLERK_SECRET_KEY}`)
+    headers.append('Content-Type', 'application/json')
+    
+
+    return await fetch(`https://api.clerk.com/v1/users/${userId}`, {
+        method: "DELETE",
+        headers: headers,
+    })
+        .then(_ => res.status(200).json({ message: 'OK' }))
+        .catch(_ => res.status(500))
+    
 }
