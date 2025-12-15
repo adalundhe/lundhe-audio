209d8f7df723d2d96ca5c073e2c002e572a7909e
Ada Lündhé <ada@datavant.com>
2025-12-14 22:36:33 -0600

    Update delete.ts

--- src/actions/user/delete.ts
+++ src/actions/user/delete.ts
@@ -1,19 +1,24 @@
 "use server"
 import { auth } from "@clerk/nextjs/server"
 import { env } from '~/env'
+import { redirect } from "next/navigation"
 
 export const deleteAccount = async () => {
     const { userId } = await auth()
     if (!userId) {
-        throw new Error("User must be authenticated to delete account")
+        redirect("/sign-in")
     }
 
     const headers = new Headers()
     headers.append("Authorization", `Bearer ${env.CLERK_SECRET_KEY}`)
     headers.append('Content-Type', 'application/json')
 
-    await fetch(`https://api.clerk.com/v1/users/${userId}`, {
-        method: "DELETE",
-        headers: headers,
-    })
+    try {
+        await fetch(`https://api.clerk.com/v1/users/${userId}`, {
+            method: "DELETE",
+            headers: headers,
+        })
+    } catch (_) {
+        console.error("User delete failed")
+    }
 }
